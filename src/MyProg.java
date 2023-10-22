4044d08d827ce62613039873ee04ae082f61f5ad
bwk5140 <brianwkarimi@gmail.com>
2023-10-22 09:00:05 +0300

    Amended independentPaths method to print all independent paths and included program comments.

--- src/MyProg.java
+++ src/MyProg.java
@@ -19,6 +19,6 @@ public class MyProg
         int [][]values = {{1, 2}, {2, 3}, {3}, {}};
         StructuralTestingTool graph = new StructuralTestingTool(4, 5, values);
         System.out.println("\nTotal number of tests = " + graph.getNumTests());
-        graph.visited(0, new Boolean[4]);
+        graph.visited(0, new Boolean[4]);   //the boolean array corresponds to the number of vertices
     }
 }

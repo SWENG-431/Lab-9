af6d76df9720eebc9de947b75d80b3dd4caffd42
bwk5140 <brianwkarimi@gmail.com>
2023-10-22 04:43:59 +0300

    Added two new methods and completely redid independentPaths.

--- src/MyProg.java
+++ src/MyProg.java
@@ -4,9 +4,9 @@ public class MyProg
 {
     public static void main(String[] args)
     {
-        int [][]values = {{1,2},{2,3},{3},{4}};
+        int [][]values = {{1, 2}, {2, 3}, {3}, {}};
         StructuralTestingTool graph = new StructuralTestingTool(4, 5, values);
         System.out.println("\nTotal number of tests = " + graph.getNumTests());
-        graph.independentPaths();
+        graph.visited(0, new Boolean[4]);
     }
 }
--- src/StructuralTestingTool.java
+++ src/StructuralTestingTool.java
@@ -1,11 +1,16 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class StructuralTestingTool
 {
     int vertices;
     int edges;
     int rows;
+    int maxEdges;
     int[][] graph;
+    Integer[][] adjacentSets;
+    int adjSetsLength;
+    String[] paths;
 
     public StructuralTestingTool()
     {
@@ -18,6 +23,9 @@ public class StructuralTestingTool
         this.vertices = vertices;
         this.graph = graph;
         this.rows = vertices;
+        setMaxEdges();
+        adjacentSets = new Integer[vertices * maxEdges][];
+        paths = getPathVariables();
     }
 
     public int getNumTests()
@@ -31,30 +39,85 @@ public class StructuralTestingTool
         return paths;
     }
 
-    public void independentPaths()
+    public void independentPaths(int i, Boolean[] visited)
     {
         int prevIteration,
-                remainingEdges = edges;
+                remainingEdges = edges,
+                tests = getNumTests() - 1;
 
-        String[] paths = getPathVariables();
+        for (int edge = 0; edge < graph[i].length; edge++)
+        {
+            if (!visited[i])
+            {
+                adjacentSets[adjSetsLength] = new Integer[]{i, graph[i][edge]};
+                adjSetsLength++;
 
+                if (i == 0)
+                {
+                    paths[tests] = Integer.toString(i) + " -> " + graph[i][edge];
+                    tests--;
+                }
+                else
+                {
+                    boolean found = false;
+                    for (Integer[] arr : adjacentSets)
+                    {
+                        if(arr != null)
+                        {
+                            for (Integer arrVal : arr)
+                            {
+                                if (arrVal.equals(i) && paths[tests].contains(Integer.toString(i)))
+                                {
+                                    if (paths[tests].contains(Integer.toString(graph[i][edge])))
+                                    {
+                                        if (paths[tests - 1].contains(Integer.toString(i)))
+                                        {
+                                            break;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        paths[tests] += " -> " + graph[i][edge];
+                                        found = true;
+                                        break;
+                                    }
+                                }
+                                else if (arrVal.equals(i) && paths[tests - 1].contains(Integer.toString(i)))
+                                {
+                                    paths[tests - 1] += " -> " + graph[i][edge];
+                                    found = true;
+                                    break;
+                                }
+                            }
+                        }
+                        if (found)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        visited[i] = true;
+/*
         for (int r = 0; r < this.rows; r++)
         {
             prevIteration = r;
             for (int c = 0; c < graph[r].length; c++)
             {
-                if (remainingEdges > 0) //Find all vertices adjacent to first vertex
+                if (remainingEdges > 0 && tests < getNumTests()) //Find all vertices adjacent to first vertex
                 {
                     if (r == 0)
                     {
-                        paths[r + c] = r + " -> " + graph[r][c];
+                        paths[tests] = r + " -> " + graph[r][c];
+                        tests++;
                         remainingEdges--;
-                    }
-                    else    //Find a consecutive path connecting all vertices from origin to terminal vertex.
+                    } else
                     {
                         prevIteration--;
                         for (int i = 0; i < graph[prevIteration].length; i++)
                         {
+                            //Find a consecutive path connecting all vertices from origin to terminal vertex.
                             if (graph[r][c] == graph[prevIteration][i] + 1 && r < this.rows - 1)
                             {
                                 if (!paths[0].contains(Integer.toString(graph[r][c])))
@@ -66,6 +129,34 @@ public class StructuralTestingTool
                         }
                         prevIteration++;
                     }
+                }
+            }
+
+            //Find all other paths
+            int index = 0;
+            for (String component : paths)
+            {
+                for (int[] components : graph)
+                {
+                    for (int values : components)
+                    {
+                        if (tests >= getNumTests())
+                        {
+                            tests--;
+                        }
+                        if (component != null && !component.contains(index + " -> " + Integer.toString(values)))
+                        {
+                            if (index < this.rows - 1)
+                            {
+                                paths[tests] = index + " -> " + values;
+
+                                if ()
+                            }
+                            else
+                                break;
+                        }
+                    }
+                    index++;
                 }
             }
         }
@@ -86,6 +177,18 @@ public class StructuralTestingTool
         }
 
         */
+    }
+
+    public void visited(int i, Boolean[] visited)
+    {
+        for (int index = 0; index < visited.length; index++)
+        {
+            visited[index] = false;
+        }
+        for (int index = i; index < visited.length; index++)
+        {
+            independentPaths(index, visited);
+        }
 
         System.out.println("******************************* ");
         System.out.println("-**Independent testing paths**- ");
@@ -95,5 +198,25 @@ public class StructuralTestingTool
         }
         System.out.println("\n******************************* ");
     }
+
+    public void setMaxEdges()
+    {
+        for (int rows = 0; rows < this.rows; rows++)
+        {
+            for (int edges = 0; edges < graph[rows].length; edges++)
+            {
+                if (rows == 0)
+                {
+                    maxEdges = graph[rows].length;
+                }
+
+                if (maxEdges < graph[rows].length)
+                {
+                    maxEdges = graph[rows].length;
+                }
+            }
+        }
+    }
+
 
 }
